e590600b5af51acf701d08ea29fce918b473f6fe
Aditya Monga <admonga@ti.com>
2010-08-27 03:16:34 -0500

    proxy: Increase limit on max buffers.
    Increase the limit on max buffers per component since the
    requirements for the latest H.264 Decoder exceeds the present limit.
    
    Signed-off-by: Aditya Monga <admonga@ti.com>
    Signed-off-by: Daniel Diaz <ddiaz@ti.com>

--- system/domx/omx_proxy_common.h
+++ system/domx/omx_proxy_common.h
@@ -70,7 +70,7 @@ extern "C"
 #define OMX_VER_MAJOR 0x1
 #define OMX_VER_MINOR 0x1
 
-#define MAX_NUM_PROXY_BUFFERS             25
+#define MAX_NUM_PROXY_BUFFERS             32
 #define MAX_COMPONENT_NAME_LENGTH         128
 #define PROXY_MAXNUMOFPORTS               8
 
